c894e874afc05445333d6a3e93bfd0bc0cd27571
xiajibazuo <2460125883@qq.com>
2026-05-10 20:47:11 +0800

    更新 mod.js

--- js/mod.js
+++ js/mod.js
@@ -58,7 +58,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.ED.points.gte(new Decimal("1"))
 }
 
 
