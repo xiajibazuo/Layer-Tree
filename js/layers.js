45a1bf90eb4128e21dfe6740c3a0d6c04ed5a0c9
xiajibazuo <2460125883@qq.com>
2026-05-10 20:46:25 +0800

    更新 layers.js

--- js/layers.js
+++ js/layers.js
@@ -131,3 +131,49 @@ addLayer("L", {
     },
     layerShown(){return player.L.points.gte(1)}
 })
+addLayer("ED", {
+    startData() { return {                  // startData is a function that returns default data for a layer. 
+        unlocked: true,                     // You can add more variables here to add them to your layer.
+        points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
+    }},
+
+    color: "#808080",                       // The color for this layer, which affects many elements.
+    resource: "结束",            // The name of this layer's main prestige resource.
+    row: 114514,                                 // The row this layer is on (0 is the first row).
+
+    baseResource: "static",                 // The name of the resource your prestige gain is based on.
+    baseAmount() { return player.points },  // A function to return the current amount of baseResource.
+
+    requires: new Decimal(0),              // The amount of the base needed to  gain 1 of the prestige currency.
+                                            // Also the amount required to unlock the layer.
+
+    type: "normal",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
+    base(){
+    return new Decimal(1)
+    },
+    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
+        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
+    },
+    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
+        return new Decimal(1)
+    },
+    tabFormat: {
+    "说明": {
+        content: [
+        "main-display",
+          "blank",
+        ["prestige-button",function(){return ""}],
+        "blank",
+        "resource-display",
+        "blank",
+        "blank",
+        ["display-text",function(){
+          let s=""
+          s+="重置会重置一切<br>"
+          return s
+        }]]
+    }
+},
+    layerShown() { return player.L.points.gte(2)}          // Returns a bool for if this layer's node should be visible in the tree.
+})
