a038af0069a8c7882eed9d52f73d4d5bebd04d77
xiajibazuo <2460125883@qq.com>
2026-05-16 22:26:08 +0800

    更新 layers.js

--- js/layers.js
+++ js/layers.js
@@ -190,7 +190,7 @@ addLayer("ED", {
     baseResource: "points",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.points },  // A function to return the current amount of baseResource.
 
-    requires: new Decimal("10^^1.78e308"),              // The amount of the base needed to  gain 1 of the prestige currency.
+    requires: new Decimal("0"),              // The amount of the base needed to  gain 1 of the prestige currency.
                                             // Also the amount required to unlock the layer.
 
     type: "none",                         // Determines the formula used for calculating prestige currency.
