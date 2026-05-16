5e067d6ecc44ce65d4d96f568437dd865ce770fd
xiajibazuo <2460125883@qq.com>
2026-05-16 14:58:45 +0800

    更新 mod.js

--- js/mod.js
+++ js/mod.js
@@ -22,7 +22,7 @@ let changelog = `<h1>Changelog:</h1><br>
 		- Added stuff.<br>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
   -test:<br>
-  <h1>1</h1>`
+  <h1>114514</h1>`
 
 let winText = `恭喜！您已成功完成游戏并到达终点，不过目前......不妨找找彩蛋( `
 
@@ -49,7 +49,7 @@ function getPointGen() {
 	    if (hasMilestone("p",0))gain=gain.times(7)
 	    else gain=gain.times(4)
 	}
-	if (hasUpgrade("p",12))gain=gain.times(Math.log(player.points.add(1)).add(1))
+	if (hasUpgrade("p",12))gain=gain.times(player.points.add(1).log(2.718281828).add(1))
 	if (hasUpgrade("p",13))gain=gain.times(player.points.pow(0.5))
 	return gain
 }
