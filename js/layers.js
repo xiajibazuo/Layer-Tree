da53690b4a5cc0da32332dc995c542388325fd5b
xiajibazuo <2460125883@qq.com>
2026-06-19 22:39:03 +0800

    更新 layers.js

--- js/layers.js
+++ js/layers.js
@@ -8,7 +8,10 @@ addLayer("L", {
 		layerPoint: new Decimal(0)
     }},
     color: "#7FAFFF",
-    requires: new Decimal(1), // Can be a function that takes requirement increases into account
+    requires(){
+        if (hasUpgrade("P",91)) return new Decimal(0.5)
+        return new Decimal(1)
+        }, // Can be a function that takes requirement increases into account
     resource: "层级", // Name of prestige currency
     baseResource: "层级点数", // Name of resource prestige is based on
     baseAmount() {return player.L.layerPoint}, // Get the current amount of baseResource
@@ -37,7 +40,7 @@ addLayer("L", {
         milestones: {
         1: {
             requirementDescription: "0层级",
-            effectDescription: "自动购买层级，每一个层级都会解锁一个层级，上限为?个层级",
+            effectDescription: "自动购买层级，每一个层级都会解锁一个层级，上限为5个层级",     //别忘了改!!!!!!!!!!!!!!!!!!!!!!!
             done() { return player.L.points.gte(0) }
         },
         2: {
@@ -162,7 +165,7 @@ addLayer("L", {
         keep.push("points")
         keep.push("milestones")
         }
-        if ((layers[resettingLayer].row > this.row || resettingLayer == "L" )&& resettingLayer !== "P") {layerDataReset(this.layer, keep)}
+        if (((resettingLayer == "ED" || getClickableState("ED",11) == 1 ) || resettingLayer == "L" )&& resettingLayer !== "P") {layerDataReset(this.layer, keep)}
     },
     layerShown(){return player.L.points.gte(1)}
 })
@@ -193,6 +196,43 @@ addLayer("ED", {
     gainExp() {                             // Returns the exponent to your gain of the prestige resource.
         return new Decimal(1)
     },
+    branches: ["L"],
+    
+    clickables: {
+    11: {
+        title: "重置层级",
+        display() {
+            if (getClickableState("ED",11) == 1) return "是"
+            else return "否"
+            },
+        onClick(){
+            if (getClickableState("ED",11) == 0) setClickableState("ED",11,1)
+            else setClickableState("ED",11,0)
+        }
+    },
+    12: {
+        title: "重置点数",
+        display() {
+            if (getClickableState("ED",12) == 1) return "是"
+            else return "否"
+            },
+        onClick(){
+            if (getClickableState("ED",12) == 0) setClickableState("ED",12,1)
+            else setClickableState("ED",12,0)
+        }
+    },
+    21: {
+        title: "重置声望点数",
+        display() {
+            if (getClickableState("ED",21) == 1) return "是"
+            else return "否"
+            },
+        onClick(){
+            if (getClickableState("ED",21) == 0) setClickableState("ED",21,1)
+            else setClickableState("ED",21,0)
+        }
+    }
+    },
     tabFormat: {
     "说明": {
         content: [
@@ -203,9 +243,10 @@ addLayer("ED", {
         "blank",
         ["display-text",function(){
           let s=""
-          s+="重置会重置一切<br>"
+          s+="选择结束是否重置以下层级<br>"
           return s
-        }]]
+        }],
+        "clickables"]
     }
 },
     layerShown() { return player.L.points.gte(4)}          // Returns a bool for if this layer's node should be visible in the tree.
@@ -221,6 +262,7 @@ addLayer("p", {
     color: "#FF0000",                       // The color for this layer, which affects many elements.
     resource: "点数",            // The name of this layer's main prestige resource.
     row: 0,                                 // The row this layer is on (0 is the first row).
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
 
     baseResource: "points",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.points },  // A function to return the current amount of baseResource.
@@ -241,14 +283,17 @@ addLayer("p", {
     player.p.points = player.points
     },
     layerShown() { return player.L.points.gte(2)},          // Returns a bool for if this layer's node should be visible in the tree.
+    branches: ["L"],
+    
     upgrades: {
         11: {
             title: "1",
             description(){
-                if(hasUpgrade("P",13))return "增益点数<br>公式：^ee45e4"
+                if(hasUpgrade("P",13))return "增益点数<br>公式：^1.3"
                 if(hasUpgrade("p",13))return "增益点数<br>公式：*7"
                 else return "增益点数<br>公式：*4"
             },
+            unlocked(){return true},
             cost: new Decimal(300)
         },
         12: {
@@ -256,29 +301,33 @@ addLayer("p", {
             description(){
                 return "点数增益自己<br>公式：*(ln(点数+1)+1)<br>效果：*" + format(player.points.add(1).log(2.718281828).add(1))
             },
+            unlocked(){return hasUpgrade("p",11)},
             cost: new Decimal(1200)
         },
         13: {
             title: "3",
             description(){
-                if(hasUpgrade("P",13)) return "升级1的效果更好(*4→^ee45e4)"
+                if(hasUpgrade("P",13)) return "升级1的效果更好(*4→^1.3)"
                 return "升级1的效果更好(*4→*7)"
             },
+            unlocked(){return hasUpgrade("p",12)},
             cost: new Decimal(24000)
         },
         14: {
             title: "4",
             description(){
-                return "点数增益自己<br>公式：*点数^1/4+1<br>效果：*" + format(player.points.pow(0.25).add(1))
+                return "增益点数<br>公式：^1.5"
             },
+            unlocked(){return hasUpgrade("p",13)},
             cost: new Decimal(40000)
         },
         15: {
             title: "5",
             description(){
-                return "点数增益自己<br>公式：*点数^1/4+1<br>效果：*" + format(player.points.pow(0.25).add(1))
+                return "增益点数<br>公式：^1.5"
             },
-            cost: new Decimal(1e6)
+            unlocked(){return hasUpgrade("p",14)},
+            cost: new Decimal(5e6)
         }
     },
     tabFormat: {
@@ -287,6 +336,11 @@ addLayer("p", {
         "main-display",
         "blank",
         "blank",
+        ["display-text",function(){
+          let s=""
+          s+="升级不消耗点数(才不是因为不会写代码呢)<br>"
+          return s
+        }],
         "upgrades"]
         }
     },
@@ -294,7 +348,7 @@ addLayer("p", {
         let keep = [];
         if (hasMilestone("P",1)) keep.push("upgrades")
         
-        if (layers[resettingLayer].row > this.row || resettingLayer == "L") {layerDataReset(this.layer, keep)}
+        if ((resettingLayer == "ED" || getClickableState("ED",12) == 1 ) || resettingLayer == "L") {layerDataReset(this.layer, keep)}
     }
 })
 addLayer("P", {
@@ -325,7 +379,9 @@ addLayer("P", {
     },
 
     layerShown() { return player.L.points.gte(3) },          // Returns a bool for if this layer's node should be visible in the tree.
-
+    branches: ["L","p"],
+    tooltip: "达到1e14点数解锁层级(关于层级要二次解锁这件事)",
+    
     milestones: {
         1: {
             requirementDescription: "10声望点数",
@@ -346,18 +402,21 @@ addLayer("P", {
             description(){
                 return "解锁声望点数的层级可购买"
             },
+            unlocked(){return hasUpgrade("P",11)},
             cost: new Decimal(10)
         },        
         13: {
             title: "P3",
             description(){
-                return "升级3的效果更好(*7→^ee45e4)"
+                return "升级3的效果更好(*7→^1.3)"
             },
+            unlocked(){return hasUpgrade("P",12)},
             cost: new Decimal(1000)
         },
         91: {
             title: "点不到的升级",
             description(){
+                if (hasUpgrade("P",91)) return "层级更便宜"
                 return "棍母"
             },
             canAfford(){return player.P.total.lte(0)},
@@ -388,5 +447,58 @@ addLayer("P", {
         "blank",
         "upgrades"]
     }
+    },
+    doReset(resettingLayer) {
+        let keep = [];
+        
+        if ((resettingLayer == "ED" || getClickableState("ED",13) == 1 ) || resettingLayer == "L") {layerDataReset(this.layer, keep)}
+    }
+})
+addLayer("GM", {
+    startData() { return {                  // startData is a function that returns default data for a layer. 
+        unlocked: true,                     // You can add more variables here to add them to your layer.
+        points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
+    }},
+
+    color: "#FFFFFF",                       // The color for this layer, which affects many elements.
+    resource: "棍母",            // The name of this layer's main prestige resource.
+    row: 0,                                 // The row this layer is on (0 is the first row).
+
+    baseResource: "棍母",                 // The name of the resource your prestige gain is based on.
+    baseAmount() { return player.points },  // A function to return the current amount of baseResource.
+
+    requires: new Decimal(10),              // The amount of the base needed to  gain 1 of the prestige currency.
+                                            // Also the amount required to unlock the layer.
+
+    type: "none",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
+
+    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
+        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
+    },
+    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
+        return new Decimal(1)
+    },
+
+    layerShown() { return player.L.points.gte(5) },          // Returns a bool for if this layer's node should be visible in the tree.
+    milestones: {
+        1: {
+            requirementDescription: "-1棍母",
+            effectDescription: "获得更多棍母(?)",
+            done() { return player.GM.points.lte(-1) }
+        }
+    },
+    tabFormat: {
+    "棍母": {
+        content: [
+        ["display-text",function(){
+          let s=""
+          s+="你有多少棍母?是一个,1.79e308个,还是" + format(player.points) + "个?<br>这不重要,重要的是,你怎么解锁棍母的?"
+          return s
+        }],
+        "blank",
+        "blank",
+        "milestones"]
+        }
     }
 })
