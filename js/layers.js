a5f2088481a0baef683992d2944a8bf9c7f947fc
xiajibazuo <2460125883@qq.com>
2026-07-12 14:12:49 +0800

    更新 layers.js

--- js/layers.js
+++ js/layers.js
@@ -643,7 +643,7 @@ buyables: {
         content: [
         "main-display",
           "blank",
-        "clickables"
+        "clickables",
         "blank",
         "blank",
         ["display-text",function(){
